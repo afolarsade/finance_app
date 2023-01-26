10ddc6ca5ee56de74e173292ecc2c2a0f20cc23f
afolarsade <afolayanafolasade@gmail.com>
2023-01-26 06:40:46 +0100

    error fixes

--- finance_app/src/components/CTA.jsx
+++ finance_app/src/components/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => (
     <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row 
     flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
       <div className='flex-1 flex flex-col'>
-        <h2 className={`${styles.heading2}`}>Try our service now!</h2>
+        <h2 className='font-poppins font-semibold xs:text-[30px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full'>Try our service now!</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Everything you need to accept card 
           payments and grow your businesss anywhere on the planet.</p>
       </div>
